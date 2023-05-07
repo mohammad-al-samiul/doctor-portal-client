5eefb51099abcd5d3d829b34d2f7b6dfdfb33cc4
Noob-H4cker <alsamiul123@gmail.com>
2023-05-07 15:55:59 +0600

    infocard added

---
+++ src/pages/Home/InfoCards/InfoCard/InfoCard.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const InfoCard = ({ card }) => {
+  const { name, description, icon, bgClass } = card;
+  return (
+    <div>
+      <div className={`card card-side bg-base-100 shadow-xl ${bgClass} p-6`}>
+        <figure>
+          <img src={icon} alt="Movie" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title"> {name} </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
---
+++ src/pages/Home/InfoCards/InfoCards.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import clock from '../../../Assets/icons/clock.svg';
+import marker from '../../../Assets/icons/marker.svg';
+import phone from '../../../Assets/icons/phone.svg';
+import InfoCard from './InfoCard/InfoCard';
+
+const InfoCards = () => {
+  const cardData = [
+    {
+      id: 1,
+      name: 'Opening Hours',
+      description: 'Open 9.00 am to 5.00pm',
+      icon: clock,
+      bgClass: 'bg-gradient-to-r from-secondary to-accent '
+    },
+    {
+      id: 2,
+      name: 'Visit our Location',
+      description: 'Brooklyn, NY 10036, United States',
+      icon: marker,
+      bgClass: 'bg-primary'
+    },
+    {
+      id: 3,
+      name: 'Contact Us Now',
+      description: '+000123456789',
+      icon: phone,
+      bgClass: 'bg-gradient-to-r from-accent to-secondary'
+    }
+  ];
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-white">
+      {cardData.map((card) => (
+        <InfoCard key={card.id} card={card}></InfoCard>
+      ))}
+    </div>
+  );
+};
+
+export default InfoCards;
