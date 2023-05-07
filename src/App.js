6b24c7234494822228a6fdc26ba19793a345aac8
Noob-H4cker <alsamiul123@gmail.com>
2023-05-07 10:05:44 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Doctors Portal</h1>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
+  );
+};
+
+export default App;
