3dcaf4eab85364efa9560bc4b2fc27c8462856a4
Noob-H4cker <alsamiul123@gmail.com>
2023-05-08 10:12:26 +0600

    footer added

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import footerImg from '../../../Assets/images/footer.png';
 
 const Footer = () => {
   return (
-    <div>
-      <h1>This is Footer</h1>
-    </div>
+    <footer
+      style={{ backgroundImage: `url(${footerImg})` }}
+      className="footer p-10 mt-20 bg-cover bg-no-repeat bg-center ">
+      <div>
+        <span className="footer-title">Services</span>
+        <Link to="/" className="link link-hover">
+          Branding
+        </Link>
+        <Link to="/" className="link link-hover">
+          Design
+        </Link>
+        <Link to="/" className="link link-hover">
+          Marketing
+        </Link>
+        <Link to="/" className="link link-hover">
+          Advertisement
+        </Link>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <Link to="/" className="link link-hover">
+          About us
+        </Link>
+        <Link to="/" className="link link-hover">
+          Contact
+        </Link>
+        <Link to="/" className="link link-hover">
+          Jobs
+        </Link>
+        <Link to="/" className="link link-hover">
+          Press kit
+        </Link>
+      </div>
+      <div>
+        <span className="footer-title">Legal</span>
+        <Link to="/" className="link link-hover">
+          Terms of use
+        </Link>
+        <Link to="/" className="link link-hover">
+          Privacy policy
+        </Link>
+        <Link to="/" className="link link-hover">
+          Cookie policy
+        </Link>
+      </div>
+    </footer>
   );
 };
 
